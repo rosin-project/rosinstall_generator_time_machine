153b9f6138661e8c1ed70311ad742bf68785244f
gavanderhoorn <g.a.vanderhoorn@tudelft.nl>
2019-02-20 15:35:12 +0100

    Make sure we build image in the correct directory.

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 VERSION="03"
-docker build -t robust-rosin/rosinstall_generator_time_machine:${VERSION} --build-arg BUILD_VERSION=${VERSION} .
+SCRIPT_DIR=$(dirname $(readlink -f $0))
+docker build -t robust-rosin/rosinstall_generator_time_machine:${VERSION} --build-arg BUILD_VERSION=${VERSION} ${SCRIPT_DIR}
